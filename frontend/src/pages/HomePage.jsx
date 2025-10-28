f96e7d3cb567d949a1e09aefbd93000f554883ee
Karan Bisht <karanbisht4041@gmail.com>
2025-10-28 12:53:44 +0530

    samlet

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div>
      HomePage 
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nihil saepe labore hic eligendi error sit consequuntur! Placeat veniam exercitationem sit perspiciatis accusantium quidem aspernatur deleniti perferendis voluptatum, quaerat eos?
+     
     </div>
   )
 }

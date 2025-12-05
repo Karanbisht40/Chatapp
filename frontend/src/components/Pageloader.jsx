f38d6e7aa2ac1842f72650db92f119256519bd6a
Karan Bisht <karanbisht4041@gmail.com>
2025-12-05 10:36:52 +0530

    Update Pageloader.jsx

--- frontend/src/components/Pageloader.jsx
+++ frontend/src/components/Pageloader.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 const Pageloader = () => {
   return (
+    //pageloadaer
     <div className='min-h-screen flex items-center justify-center'>
       <LoaderIcon className='animate-spin size-10 text-primary' />
     </div>
